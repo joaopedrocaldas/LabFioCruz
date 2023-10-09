4acb3c4a7170d066d914d5901ef675b1fd9e1012
joaopedrocaldas <jpecaldas@gmail.com>
2023-10-09 17:03:50 -0300

    rafa salva

--- models/projeto.ts
+++ models/projeto.ts
@@ -8,15 +8,30 @@ interface Projeto {
 }
 
 class Projeto {
-	public static async listar(idestado: number): Promise<Projeto[]> {
+	public static async listar(idestado: number, idcidade: number): Promise<Projeto[]> {
 		let lista: Projeto[] = [];
+
+		let where = "";
+		let parametros: any[] = [];
+
+		if (idestado) {
+			where += (where ? " and " : " where ");
+			where += "p.idestado = ?";
+			parametros.push(idestado);
+		}
+
+		if (idcidade) {
+			where += (where ? " and " : " where ");
+			where += "p.idcidade = ?";
+			parametros.push(idcidade);
+		}
 
 		await app.sql.connect(async (sql: app.Sql) => {
 			lista = await sql.query(`
 			SELECT p.id, p.nome, p.autor, sigla FROM projeto p
 				INNER JOIN estado e on p.idestado = e.id
-				WHERE idestado = ?
-				ORDER BY nome`, [idestado]);
+				${where}
+				ORDER BY nome`, parametros);
 		});
 
 		return lista;
--- routes/api/projeto.ts
+++ routes/api/projeto.ts
@@ -3,7 +3,7 @@ import Projeto = require("../../models/projeto");
 
 class ProjetoApiRoute {
     public async listar(req: app.Request, res: app.Response) {
-        res.json(await Projeto.listar(parseInt(req.query["idestado"] as string)));
+        res.json(await Projeto.listar(parseInt(req.query["idestado"] as string), parseInt(req.query["idcidade"] as string)));
     }
 }
 
